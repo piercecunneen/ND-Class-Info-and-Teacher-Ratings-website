cd3640e83db7bf2e6cc06693dbd6e8dd7af4c070
Zach Janicki <zachjanicki@Zachs-Air.attlocal.net>
2015-11-26 00:52:04 -0500

    added password.py  to add users to database and validate login info

---
+++ password.py
@@ -0,0 +1,35 @@
+import sqlite3
+from passlib.hash import pbkdf2_sha256
+
+db_path = "/Users/zachjanicki/Git/ND-Class-Info-and-Teacher-Ratings-website/pass.sqlite"
+
+def create_user(username, password):
+    
+    pass_hash = pbkdf2_sha256.encrypt(password, rounds=200, salt_size=16)
+    
+    conn = sqlite3.connect(db_path)
+    with conn:
+        c = conn.cursor()
+        sql = 'insert into passwords values("' + username + '", "' + pass_hash + '")'
+        print sql
+        c.execute(sql) 
+        
+def validate_user(username, password):
+    
+    conn = sqlite3.connect(db_path)
+    with conn:
+        c = conn.cursor()
+        sql = "select * from passwords where username = " + "'" + username + "'" 
+        c.execute(sql)
+        user = c.fetchone()
+    print user
+    
+    if user is None:
+        return False
+    
+    return pbkdf2_sha256.verify(password, user[1])
+    
+#test script
+
+x = validate_user("zj", "pass")
+print x

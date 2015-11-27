1991c298c831cce2fd7652628658ea37e8744db0
dmattia <dmattia@nd.edu>
2015-11-27 03:20:02 -0500

    Finished design of search, prof pages, and rating form

--- class_search_web_scrapping.py
+++ class_search_web_scrapping.py
@@ -8,10 +8,8 @@ def CleanUpString(string):
     """
     return string.replace('\t', '').replace('\r','').replace('\n', '').replace('  ', '')
 
-
 def GetCurrentSemester():
     return "201520"
-    
     
 def GetOptions():
     """Gets the options for the 6 categories (Term, Division, Campus, Subject, Attribute, and Credits)..
@@ -53,11 +51,6 @@ def GetOptions():
         if 'Year' in entry:
             del OptionCategories[0][entry]
     return OptionCategories
-        
-
-
-
-    
 
 def GetClasses(term, subj, credit, Attr, divs, campus):
     """
@@ -167,9 +160,6 @@ def GetClasses(term, subj, credit, Attr, divs, campus):
     
         return Classlist
 
-
-
-
 def GetClassDescriptionAndAll(CRN, Term):
     """Gets the class description, the course prerequisites, and the course corequisites
     Input: a url of a class specific page
@@ -210,9 +200,6 @@ def GetClassDescriptionAndAll(CRN, Term):
     else:
         return [Course_Description, 'Neither', AttributeText, Restrictions]
 
-
-
-
 def Sort_dict(data, isTerms):
     """ Takes the keys in a dictionary, sorts them by their corresponding value, and then puts
     the keys in an ordered list. For the Terms, want highest numbers first, so need to reverse the keys list"""
@@ -223,7 +210,6 @@ def Sort_dict(data, isTerms):
         return keys
     else:
         return sorted(data)
-
 
 #CollegeDepartments = []
 #for i in Colleges:
@@ -260,10 +246,6 @@ def GetSubjectsInDepartments():
         sorted_Colleges_with_deparments.append(new_college)
     f.close()
     return sorted_Colleges_with_deparments
-    
-
-
-
 
 def GetAllProfessors():
     f = open('TeacherList.txt', 'r')
@@ -317,7 +299,3 @@ def GetCoursesTaught(Prof_ID):
         url_data = url_data.split('CRN=')[1].split('TERM=')
         CoursesTaught.append(course.text.split('\n')[1:-1] + url_data)
     return CoursesTaught
-    
-
-
-    

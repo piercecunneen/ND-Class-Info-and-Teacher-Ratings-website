fa5319eb5ecf6b79eb5ab9095e9fb5df76f36a7d
Zach Janicki <zachjanicki@zachs-air.dhcp.nd.edu>
2015-12-01 00:39:56 -0500

    adding untracked files

---
+++ User.py
@@ -0,0 +1,24 @@
+from flask.ext.login import LoginManager, UserMixin, login_required
+
+class User(UserMixin):
+	user_database = {"pcunneen@nd.edu": ("pcunneen@nd.edu", "5BrnH+"), "JaneDoe": ("Jane", "Doe")}
+
+
+	def __init__(self, username, password):
+		self.id = username
+		self.password = password
+		self.authenticated = True
+		self.active = True
+
+
+	@classmethod
+	def get(cls, id):
+		
+		if cls.user_database.get(id):
+			return User(*cls.user_database.get(id))
+		else:
+			return None
+
+	
+
+

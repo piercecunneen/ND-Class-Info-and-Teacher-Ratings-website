d27594b8ac2490907e5317a2dea82363a8cd7573
dmattia <dmattia@nd.edu>
2015-12-20 19:21:10 -0500

    Refactoring Style

--- User.py
+++ User.py
@@ -1,24 +1,18 @@
 from flask.ext.login import LoginManager, UserMixin, login_required
 
 class User(UserMixin):
-	user_database = {"pcunneen@nd.edu": ("pcunneen@nd.edu", "5BrnH+"), "JaneDoe": ("Jane", "Doe")}
+    user_database = {"pcunneen@nd.edu": ("pcunneen@nd.edu", "5BrnH+"), "JaneDoe": ("Jane", "Doe")}
 
+    def __init__(self, username, password):
+        self.id = username
+        self.password = password
+        self.authenticated = True
+        self.active = True
 
-	def __init__(self, username, password):
-		self.id = username
-		self.password = password
-		self.authenticated = True
-		self.active = True
+    @classmethod
+    def get(cls, id):
 
-
-	@classmethod
-	def get(cls, id):
-		
-		if cls.user_database.get(id):
-			return User(*cls.user_database.get(id))
-		else:
-			return None
-
-	
-
-
+        if cls.user_database.get(id):
+            return User(*cls.user_database.get(id))
+        else:
+            return None

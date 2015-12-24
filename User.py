15fb04d9935261b4962e83610780ed19cfa11f24
piercecunneen <pcunneen19@gmail.com>
2015-12-23 23:09:44 -0500

    changes to login (added Remember me)

--- User.py
+++ User.py
@@ -1,18 +1,30 @@
 from flask.ext.login import LoginManager, UserMixin, login_required
-
+import sqlite3 as lite
+database = 'reviews.sqlite'
 class User(UserMixin):
-    user_database = {"pcunneen@nd.edu": ("pcunneen@nd.edu", "5BrnH+"), "JaneDoe": ("Jane", "Doe")}
-
-    def __init__(self, username, password):
+    def __init__(self, username):
         self.id = username
-        self.password = password
         self.authenticated = True
         self.active = True
 
     @classmethod
     def get(cls, id):
-
-        if cls.user_database.get(id):
-            return User(*cls.user_database.get(id))
+        if Check_For_ID(id):
+            print "Boom"
+            return User(id)
         else:
             return None
+
+
+def Check_For_ID(id):
+    conn = lite.connect(database)
+    with conn:
+        c = conn.cursor()
+        query = "select username from userInfo where username = " + "'" + str(id) + "'"
+        c.execute(query)
+        profReviews = c.fetchall()
+        if profReviews:
+            return True
+        else:
+            return False
+

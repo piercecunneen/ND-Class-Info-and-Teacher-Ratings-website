3052d0d48c9e5aae36b904b145bc26b249bd5188
dmattia <dmattia@nd.edu>
2015-12-23 23:45:35 -0500

    Fixed search page

--- routes.py
+++ routes.py
@@ -482,4 +482,5 @@ def message_board():
     return render_template('message_board.html', all_posts=getPosts())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)

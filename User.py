e8d0342717e305994bda5871c63a32bccea6467e
piercecunneen <pcunneen19@gmail.com>
2015-12-24 17:02:16 -0500

    changes to user login by username and email

--- User.py
+++ User.py
@@ -8,18 +8,21 @@ class User(UserMixin):
         self.active = True
 
     @classmethod
-    def get(cls, id):
-        if Check_For_ID(id):
+    def get(cls, id, is_email):
+        if Check_For_ID(id, is_email):
             return User(id)
         else:
             return None
 
 
-def Check_For_ID(id):
+def Check_For_ID(id, is_email):
     conn = lite.connect(database)
     with conn:
         c = conn.cursor()
-        query = "select username from userInfo where username = " + "'" + str(id) + "'"
+        if is_email:
+            query = "select username from userInfo where email = " + "'" + str(id) + "'"
+        else:
+            query = "select username from userInfo where username = " + "'" + str(id) + "'"
         c.execute(query)
         profReviews = c.fetchall()
         if profReviews:

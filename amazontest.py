2f27a5e340adfd879ae5f0ee2e45fa0c2c8a0202
piercecunneen <pcunneen19@gmail.com>
2015-12-26 13:52:17 -0500

    For reviews, changed department to go by the class rather than by professor

--- amazontest.py
+++
@@ -1,8 +0,0 @@
-from amazonproduct import API
-api = API(locale='de')
-
-# get all books from result set and
-# print author and title
-for book in api.item_search('Books', Publisher='Galileo Press'):
-    print '%s: "%s"' % (book.ItemAttributes.Author,
-                        book.ItemAttributes.Title)
